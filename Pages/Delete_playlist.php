b3c71686cd998631a9a532043756f2cd55bab8de
Cool <evan.ashley11@gmail.com>
2025-05-30 13:18:41 +0700

    update deleting playlist like

--- Pages/Delete_playlist.php
+++ Pages/Delete_playlist.php
@@ -11,6 +11,8 @@ $userId = $_SESSION['id_user'];
 $playlistId = $_GET['id'] ?? null;
 
 if ($playlistId) {
+    mysqli_query($connect, "DELETE FROM playlist_likes WHERE id_playlist = $playlistId");
+
     mysqli_query($connect, "DELETE FROM playlists_songs WHERE id_playlist = $playlistId");
     
     mysqli_query($connect, "DELETE FROM playlists WHERE id_playlist = $playlistId AND id_user = $userId");
